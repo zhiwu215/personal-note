2a9860a17fec749c12ebb15706250e5351c534f6
zhiwu <1129665454@qq.com>
2026-08-07 23:06:32 +0800

    feat: 窗口设置

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,9 +6,10 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 800,
+    height: 600,
     show: false,
+    frame: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
